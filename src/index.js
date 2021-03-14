4bd1384befe12cd934017261a98cf0df7095c32a
arisa <arisa.pirata@gmail.com>
2021-03-13 18:08:24 -0800

    Creating Your Own GraphQL API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-import myCurrentLocation, { getGreeting, message, name } from './myModule';
-import myAddFunction, { subtract } from './math'
+import { GraphQLServer } from 'graphql-yoga';
 
-console.log(message);
-console.log(name);
-console.log(myCurrentLocation);
-console.log(getGreeting('Jessica'));
+//  Type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`
 
-console.log(myAddFunction(1, 1));
-console.log(subtract(5, 1));
+//  Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+        return 'This is my first query!'
+        },
+        name() {
+          return 'Arisa Hirata'
+        },
+        location() {
+            return 'Vancouver'
+        },
+        bio() {
+            return 'I am from Japan.'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log('The server is up!');
+});
